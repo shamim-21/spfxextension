7bbf3ca4f5a4ef00088578acd4c0db28d323cf6a
shamim-21 <50737807+shamim-21@users.noreply.github.com>
2021-02-16 11:00:28 +0530

    Add files via upload

---
+++ lib/extensions/jaguarextension/AppCustomizer.module.scss.d.ts
@@ -0,0 +1,8 @@
+declare const styles: {
+    app: string;
+    top: string;
+    logo: string;
+    bottom: string;
+};
+export default styles;
+//# sourceMappingURL=AppCustomizer.module.scss.d.ts.map
